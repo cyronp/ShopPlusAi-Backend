74bf5bf3b21a26a5bad7c417bf08e43d59b3c45e
Caio <caiodesouza.cds@gmail.com>
2026-04-25 12:02:17 -0300

    feat: criado metodo de deletar usuario no usuario service

--- src/main/java/com/univille/api/shopplusai/domain/usuario/UsuarioService.java
+++ src/main/java/com/univille/api/shopplusai/domain/usuario/UsuarioService.java
@@ -2,6 +2,7 @@ package com.univille.api.shopplusai.domain.usuario;
 
 import com.univille.api.shopplusai.domain.usuario.dto.CreateUsuarioRequest;
 import com.univille.api.shopplusai.domain.usuario.dto.UsuarioResponse;
+import com.univille.api.shopplusai.infra.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,4 +17,10 @@ public class UsuarioService {
         repository.save(usuario);
         return new UsuarioResponse(usuario);
     }
+
+    public void delete(Long id){
+        var usuario = repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Usuário não encontrado"));
+    }
+    
 }
