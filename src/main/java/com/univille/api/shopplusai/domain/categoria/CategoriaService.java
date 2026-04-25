ad1483e8de8174f4a5e62cde47ebfec2955f81c2
Caio <caiodesouza.cds@gmail.com>
2026-04-25 14:43:12 -0300

    feat: cria metodo de cadastrar uma categoria no service

--- src/main/java/com/univille/api/shopplusai/domain/categoria/CategoriaService.java
+++ src/main/java/com/univille/api/shopplusai/domain/categoria/CategoriaService.java
@@ -1,7 +1,9 @@
 package com.univille.api.shopplusai.domain.categoria;
 
+import com.univille.api.shopplusai.domain.categoria.dto.CreateCategoriaRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -9,5 +11,10 @@ public class CategoriaService {
 
     private final CategoriaRepository repository;
 
-    
+    @Transactional
+    public Categoria save(CreateCategoriaRequest dados){
+        var categoria = new Categoria(dados);
+        repository.save(categoria);
+        return categoria;
+    }
 }
