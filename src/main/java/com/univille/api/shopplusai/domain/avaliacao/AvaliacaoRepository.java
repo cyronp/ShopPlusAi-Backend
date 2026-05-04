79182b20363889d13639fff59f708287e57cda91
Caio <caiodesouza.cds@gmail.com>
2026-05-04 17:15:55 -0300

    feat: cria repository para avaliacoes

---
+++ src/main/java/com/univille/api/shopplusai/domain/avaliacao/AvaliacaoRepository.java
@@ -0,0 +1,6 @@
+package com.univille.api.shopplusai.domain.avaliacao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> {
+}
